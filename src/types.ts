415432c07d46f392bcd055acab772f02b9b787b1
ing.dacastillop <ing.dacastillop@gmail.com>
2024-07-22 13:23:09 -0500

    :recycle: Refactor types in Project

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,14 @@
 import { Signal } from '@angular/core';
-import { AbstractArrayControl, AbstractReactiveControl } from '@rolster/forms';
+import {
+  AbstractReactiveArrayControl,
+  AbstractReactiveControl
+} from '@rolster/forms';
 
 export interface AngularControl<T = any> extends AbstractReactiveControl<T> {
   readonly signal: Signal<T>;
 }
 
-export interface AngularArrayControl<T = any> extends AbstractArrayControl<T> {
+export interface AngularArrayControl<T = any>
+  extends AbstractReactiveArrayControl<T> {
   readonly signal: Signal<T>;
 }
