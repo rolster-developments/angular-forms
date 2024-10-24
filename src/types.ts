706fb6b0c02c68ba619af4e2aaecbc17ca2cb335
ing.dacastillop <ing.dacastillop@gmail.com>
2024-10-24 13:47:28 -0500

    :rocket: New type AngularControlEmpty

--- src/types.ts
+++ src/types.ts
@@ -8,7 +8,13 @@ export interface AngularControl<T = any> extends AbstractReactiveControl<T> {
   readonly signal: Signal<T>;
 }
 
+export type AngularControlEmpty<T = any> = AngularControl<T | undefined>;
+
 export interface AngularArrayControl<T = any>
   extends AbstractReactiveArrayControl<T> {
   readonly signal: Signal<T>;
 }
+
+export type AngularArrayControlEmpty<T = any> = AngularArrayControl<
+  T | undefined
+>;
